417afc1ad957104c7104cd9ea9dcf4436d3d5301
nycrera <nycrera@gmail.com>
2020-12-12 20:20:18 +0300

    RabbitMQ Listener and Configuration were added.

---
+++ src/main/java/com/rabbitmq/rabbitmqlistener/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package com.rabbitmq.rabbitmqlistener;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    private static final String MY_QUEUE = "MyQueue";
+
+    @Bean
+    Queue myQueue() {
+        return new Queue(MY_QUEUE, true);
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    MessageListenerContainer messageListenerContainer() {
+        SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+        simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+        simpleMessageListenerContainer.setQueues(myQueue());
+        simpleMessageListenerContainer.setMessageListener(new RabbitMQMessageListener());
+        return simpleMessageListenerContainer;
+    }
+
+}
