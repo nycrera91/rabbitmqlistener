e7e87870f5179f9efb0c14134c7d8e476107f946
nycrera <nycrera@gmail.com>
2020-12-13 15:23:19 +0300

    RabbitMQ Queue Builder was added.

--- src/main/java/com/rabbitmq/rabbitmqlistener/RabbitMQListenerApplication.java
+++ src/main/java/com/rabbitmq/rabbitmqlistener/RabbitMQListenerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqlistenerApplication {
+public class RabbitMQListenerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitmqlistenerApplication.class, args);
+        SpringApplication.run(RabbitMQListenerApplication.class, args);
     }
 
 }
